739a5ad0a12e85388e7a22f9a010c6bcd7175906
Mia Johansson <24357628+miajohansson9@users.noreply.github.com>
2025-05-13 22:18:48 -0700

    Added meld logos instead of librechat

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -44,8 +44,8 @@ export default defineConfig({
       },
       includeAssets: ['**/*'],
       manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
+        name: 'Meld',
+        short_name: 'Meld',
         start_url: '/',
         display: 'standalone',
         background_color: '#000000',
